19b00228ad9e77aaee7ed73be73f8a8fbfc4d1ea
Oliver Linnarsson <oliver@lolo.company>
2024-04-18 10:34:05 +0200

    chore: Prefer numbers over text in describe

--- test/pressure.test.ts
+++ test/pressure.test.ts
@@ -2,32 +2,32 @@ import { it, describe } from 'vitest';
 
 import { Queue } from '../src/main';
 
-const CAPACITY = 10;
+const capacity = 10;
 
 describe('pressure', () => {
-  it('should be zero when empty', async ({ expect }) => {
-    const Q = new Queue<Symbol>(CAPACITY);
+  it('should be 0 when empty', async ({ expect }) => {
+    const Q = new Queue<Symbol>(capacity);
     expect(Q.pressure()).to.equal(0);
   });
 
-  it('should be one when at capacity', async ({ expect }) => {
-    const Q = new Queue<number>(CAPACITY);
+  it('should be 1 when at capacity', async ({ expect }) => {
+    const Q = new Queue<number>(capacity);
     for (let i = 0; i < Q.capacity(); i++) {
       Q.write(i);
     }
     expect(Q.pressure()).to.equal(1);
   });
 
-  it('should be one half when at half capacity', async ({ expect }) => {
-    const Q = new Queue<number>(CAPACITY);
+  it('should be 0.5 when at half capacity', async ({ expect }) => {
+    const Q = new Queue<number>(capacity);
     for (let i = 0; i < Q.capacity() / 2; i++) {
       Q.write(i);
     }
     expect(Q.pressure()).to.equal(0.5);
   });
 
-  it('should be two when at double the capacity', async ({ expect }) => {
-    const Q = new Queue<number>(CAPACITY);
+  it('should be 2 when at double the capacity', async ({ expect }) => {
+    const Q = new Queue<number>(capacity);
     for (let i = 0; i < Q.capacity() * 2; i++) {
       Q.write(i);
     }
